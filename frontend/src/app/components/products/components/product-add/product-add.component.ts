46091b82e00105709a19cdfa75640f2aef98de94
tanersaydam <tanersaydam@gmail.com>
2023-03-18 10:50:23 +0300

    .

--- frontend/src/app/components/products/components/product-add/product-add.component.ts
+++ frontend/src/app/components/products/components/product-add/product-add.component.ts
@@ -17,7 +17,8 @@ import { CategoryModel } from '../../../categories/models/category.model';
   styleUrls: ['./product-add.component.css']
 })
 export class ProductAddComponent implements OnInit {
-  images:any;
+  images: File[] = [];
+  imageUrls: any[] = [];
   routes: RoutesModel[] = [
     {
       name: "Ana Sayfa",
@@ -37,48 +38,78 @@ export class ProductAddComponent implements OnInit {
   ]
   categories: CategoryModel[] = [];
 
-  
+
   constructor(
     private _product: ProductService,
     private _toastr: ToastrService,
     private _category: CategoryService
-  ){}
+  ) { }
 
   ngOnInit(): void {
     this.getCategories();
   }
 
-  getImages(event:any){    
-    this.images = event.target.files;
+  getImages(event: any) {
+    const file: File[] = Array.from(event.target.files);
+    this.images.push(...file);
+    //this.imageUrls = [];
+
+    for (let i = 0; i < event.target.files.length; i++) {
+      const file: File = event.target.files[i];
+
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+
+      reader.onload = () => {
+        const imageUrl = reader.result as string;
+        this.addImage(imageUrl, i);
+      };
+    }
   }
 
-  getCategories(){
-    this._category.getAll(res=>{
+  addImage(imageUrl: string, index:number) {
+    this.imageUrls.push(
+      {imageUrl: imageUrl, index: index});
+  }
+
+  removeImage(imgeIndex: number,index:number){
+    this.imageUrls.splice(index,1);
+    this.images.splice(imgeIndex,1);
+  }
+
+  getCategories() {
+    this._category.getAll(res => {
       this.categories = res;
     });
   }
 
-  add(form:NgForm){
-    if(form.valid){
+  add(form: NgForm) {
+    if(form.value["categoriesSelect"].length == 0){
+      this._toastr.error("Kategori seçimi yapmadınız!");
+      return;
+    }
+
+    if (form.valid) {
       let product = form.value;
       let categories: string[] = product["categoriesSelect"];
       let price = product["price"];
-      price = price.toString().replace(",",".");
+      price = price.toString().replace(",", ".");
       let formData = new FormData();
       formData.append("name", product["name"]);
       formData.append("price", price);
       formData.append("stock", product["stock"]);
       for (const category of categories) {
-        formData.append("categories",category);
+        formData.append("categories", category);
       }
-      formData.append("description", product["description"]);      
+      formData.append("description", product["description"]);
       for (const image of this.images) {
         formData.append("images", image, image.name);
       }
-      
-      this._product.add(formData,res=>{
+
+      this._product.add(formData, res => {
         this._toastr.success(res.message);
         form.reset();
+        this.imageUrls = [];
       });
     }
   }
